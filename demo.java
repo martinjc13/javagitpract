6603dfdc2a8f2022a1d8b6c2ba4ff0fadd7b159c
Joshua Martin <118765929+martinjc13@users.noreply.github.com>
2024-08-20 19:31:29 -0400

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,20 @@
+/*
+ * Josh Martin
+ * Omar ALchab
+ * Leah Anderson
+ * 
+ */
+import java.util.Scanner;
+public class demo implements Animal
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    @Override
+    public void noise() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'noise'");
+    }
+}
