20a4aa26b042e28a6f185da669203683124354c4
Joshua Martin <118765929+martinjc13@users.noreply.github.com>
2024-08-21 11:30:30 -0400

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,15 +1,35 @@
-/*
- * Josh Martin
- * Omar ALchab
- * Leah Anderson
+/**
+ * Demo method to run the animal interface.
  * 
+ * @version 8/21/2024
+ * @author Josh Martin
+ * @author Omar ALchab
+ * @author Leah Anderson
  */
 import java.util.Scanner;
 public class demo implements Animal
 {
     public static void main(String[] args) 
     {
-        
+        System.out.println("What animal would you like to hear?\nCat or Dog?");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        Animal animal;
+        if (s.equals("Cat") || s.toString().equals("cat"))
+        {
+            animal = new Cat();
+            animal.noise();
+        } 
+        else if (s.toString().equals("Dog") || s.toString().equals("dog"))
+        {
+            animal = new Dog();
+            animal.noise();
+        }
+        else
+        {
+            System.out.println("Please enter the right animal");
+        }
+        scan.close();
     }
 
     @Override
